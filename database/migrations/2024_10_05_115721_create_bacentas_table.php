d5d71da4e6d0cb53763517326b7d686f512ba133
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-10-07 10:50:28 +0000

    update backend server with add photo code

--- database/migrations/2024_10_05_115721_create_bacentas_table.php
+++ database/migrations/2024_10_05_115721_create_bacentas_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('councils', function (Blueprint $table) {
+        Schema::create('bacentas', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description')->nullale()->comment("This can describe what areas this council is focusing on");
+            $table->unsignedBigInteger('location_id')->references('id')->on('locations');
             $table->unsignedBigInteger('leader_id')->references('id')->on('users');
-            $table->unsignedBigInteger('stream_id')->references('id')->on('streams');
+            $table->unsignedBigInteger('council_id')->references('id')->on('councils');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('councils');
+        Schema::dropIfExists('bacentas');
     }
 };
