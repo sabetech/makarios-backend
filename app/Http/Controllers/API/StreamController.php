a8fba7f0a4b3f3789dcf315d41c436280c8fac93
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2024-11-19 17:32:24 +0000

    deploy regionalInfo

--- app/Http/Controllers/API/StreamController.php
+++ app/Http/Controllers/API/StreamController.php
@@ -18,6 +18,8 @@ class StreamController extends BaseController
     public function show($id) {
         $stream = Stream::find($id);
 
+        $stream->regionalInfo = $stream->regions()->with(['zones', 'bacentas'])->get();
+
         //TODO:: handle stream not found later
         return $this->sendResponse($stream, 'Stream retrieved successfully.');
     }
