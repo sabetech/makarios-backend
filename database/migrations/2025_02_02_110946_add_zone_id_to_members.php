2cd573f57416e013552ba30e5e2a13222e68eca6
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2025-02-02 12:27:52 +0000

    Fix member adding when no bacenta is selected

---
+++ database/migrations/2025_02_02_110946_add_zone_id_to_members.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('zone_id')->references('id')->on('zones')->after('region_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('members', function (Blueprint $table) {
+            //
+            $table->dropColumn('zone_id');
+        });
+    }
+};
