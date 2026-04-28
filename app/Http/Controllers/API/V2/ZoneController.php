75dcbb1a4c68d01a44d09358f444ba8e2372f671
sabetech <albert.mensahansah@gmail.com>
2026-04-28 17:38:12 +0000

    get zones with bacentas

--- app/Http/Controllers/API/V2/ZoneController.php
+++ app/Http/Controllers/API/V2/ZoneController.php
@@ -21,13 +21,13 @@ class ZoneController extends BaseController
         $roles = $user->roles->pluck('name')->toArray();
 
         if (in_array('Super Admin', $roles) || in_array('Bishop', $roles)) {
-            $zones = Zone::with(['region', 'stream', 'leader'])->get();
+            $zones = Zone::with(['region', 'stream', 'bacentas', 'leader'])->get();
         } elseif (in_array('Region Lead', $roles)) {
-            $zones = Zone::with(['region', 'stream', 'leader'])->whereHas('region', function($q) use ($user) {
+            $zones = Zone::with(['region', 'stream', 'leader', 'bacentas'])->whereHas('region', function($q) use ($user) {
                 $q->where('id', $user->region_id);
             })->get();
         } elseif (in_array('Zone Lead', $roles)) {
-            $zones = Zone::with(['region', 'stream', 'leader'])->whereHas('leader', function($q) use ($user) {
+            $zones = Zone::with(['region', 'stream', 'leader', 'bacentas'])->whereHas('leader', function($q) use ($user) {
                 $q->where('id', $user->id);
             })->get();
         } else {
